244ba1d5419309b7e5b6fcbef64a4b8962a5305f
Evgeny Nikishov <evgenynikishov@mail.ru>
2022-11-13 21:57:02 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.mult(2, 3));
+        System.out.println(calc.pow(6, 3));
+    }
+}
